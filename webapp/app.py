400d164c4166851eed7eee775c31060eec4bf7c2
Facundo Mazzola <facumazzola2@gmail.com>
2025-10-23 18:47:47 -0300

    Change Flask app to run on port 8080
    Updated the Flask app to run on port 8080.

--- webapp/app.py
+++ webapp/app.py
@@ -92,7 +92,8 @@ if __name__ == "__main__":
     else:
         print(f"Starting Flask app - Endpoint: {ENDPOINT_NAME}")
     
-    app.run(debug=False,host="0.0.0.0")
+    app.run(debug=False,host="0.0.0.0",port=8080)
+
 
 
 
